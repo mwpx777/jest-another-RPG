0b96a70dda4d54df829ef173d9de1485bead276f
MWPX777 <mwpx777@gmail.com>
2021-02-14 14:17:53 -0600

    completed 10.2

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -14,3 +14,22 @@ test("create a player object", ()=>{
     );
 
 });
+
+test('get a players stats as an object', () => {
+    const player = new Player('Mark');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+})
+
+test('get inventory from player or return false', ()=>{
+    const player = new Player('Mark');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory= [];
+
+    expect(player.getInventory()).toEqual(false);
+})
